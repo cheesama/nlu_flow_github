cafc9fa3e8db911d519e9011ac380aa6b13e4eba
cheesama <cheehoon12@hanmail.net>
2020-08-22 22:41:36 +0900

    rename module and domain_classifier implement start

---
+++ nlu_flow/sentence_classification/domain_classifier/readme.md
@@ -0,0 +1,7 @@
+## Domain Classifier dataset info
+
+sentence        category            label
+.....           scenario(domain)    0
+....            FAQ                 1
+...             out of domain       2
+
---
+++ nlu_flow/sentence_classification/domain_classifier/sklearn_domain_classifier.py
@@ -0,0 +1,11 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+
+from tqdm import tqdm
+from pprint import pprint
+
+import os, sys
