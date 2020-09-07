785977bcc58869fc6929d99d7126c477c79a1cc0
Ubuntu <ltech@rasalt-dev.p1nvgwc5saqe3ass43yrm55tjc.syx.internal.cloudapp.net>
2020-09-07 09:19:29 +0000

    implement start for slang classifier

---
+++ nlu_flow/sentence_classification/slang_classifier/sklearn_slang_classifier.py
@@ -0,0 +1,77 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+
+from tqdm import tqdm
+from pprint import pprint
+
+from nlu_flow.utils import meta_db_client
+from nlu_flow.preprocessor.text_preprocessor import normalize
+
+import os, sys
+import dill
+
+def train_slang_classifier():
+    # load dataset
+    scenario_table_list=['intent-rules', 'nlu-intent-entity-utterances']
+    faq_table_list=['nlu-faq-questions']
+    out_of_domain_table_list=['nlu-chitchat-utterances','nlu-slang-utterances']
+
+    utterances = []
+    labels = []
+    
+    ## scenario domain
+    for scenario_table in scenario_table_list:
+        scenario_data = meta_db_client.get(scenario_table)
+        for data in tqdm(scenario_data, desc=f'collecting table data : {scenario_table}'):
+            if 'data_type' in data.keys():
+                if data['data_type'] == 'training':
+                    if 'faq' in data['intent_id']['Intent_ID'].lower():
+                        utterances.append(normalize(data['utterance']))
+                        labels.append('faq')
+                    elif data['intent_id']['Intent_ID'] == 'intent_OOD':
+                        utterances.append(normalize(data['utterance']))
+                        labels.append('out_of_domain')
+                    elif data['intent_id']['Intent_ID'] not in ['intent_미지원']:
+                        utterances.append(normalize(data['utterance']))
+                        labels.append('scenario')
+ 
+            else:
+                utterances.append(normalize(data['utterance']))
+                labels.append('scenario')
+ 
+    ## FAQ domain
+    for faq_table in faq_table_list:
+        faq_data = meta_db_client.get(faq_table)
+        for data in tqdm(faq_data, desc=f'collecting table data : {faq_table}'):
+            utterances.append(normalize(data['question']))
+            labels.append('faq')
+
+    ## out of domain
+    for ood_table in out_of_domain_table_list:
+        ood_data = meta_db_client.get(ood_table)
+        for data in tqdm(ood_data, desc=f'collecting table data : {ood_table}'):
+            utterances.append(normalize(data['utterance']))
+            labels.append('out_of_domain')
+
+    X_train, X_test, y_train, y_test = train_test_split(utterances, labels, random_state=88)
+
+    svc = make_pipeline(CountVectorizer(analyzer="char_wb"), SVC(probability=True))
+    print("domain classifier training(with SVC)")
+    svc.fit(X_train, y_train)
+    print("model training done, validation reporting")
+    y_pred = svc.predict(X_test)
+    print(classification_report(y_test, y_pred))
+
+    with open('report.md', 'w') as reportFile:
+        print(classification_report(y_test, y_pred), file=reportFile)
+
+    #save domain classifier model
+    with open('domain_classifier_model.svc','wb') as f:
+        dill.dump(svc, f)
+        print ('domain_classifier model saved : domain_classifier_model.svc')
+
+train_domain_classifier()
--- nlu_flow/utils/kor_char_tokenizer.py
+++ nlu_flow/utils/kor_char_tokenizer.py
@@ -1,4 +1,54 @@
+from tqdm import tqdm
+
+import os, sys
+import re
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from preprocessor.text_preprocessor import normalize
+
 class KorCharTokenizer:
-    def __init__(self):
-        pass
+    '''
+    a ~ z : 97 ~ 122
+    가 ~ 힣 : 44032 ~ 55203
+    '''
+    def __init__(self, max_len=128, padding=True):
+        self.max_len = 128
+        self.padding = padding
+
+        self.char_dict = {}
+        self.char_dict[0] = '[CLS]'
+        self.char_dict[1] = '[SEP]'
+        self.char_dict[2] = '[PAD]'
+        self.char_dict[3] = '[UNK]'
+        self.char_dict[4] = ' '
+
+        # a ~ z mapping
+        for i in range(97, 123):
+            self.char_dict[len(self.char_dict)] = chr(i)
+
+        # 가 ~ 힣 mapping
+        for i in tqdm(range(44032, 55204), desc='building korean char_token dictionary...'):
+            self.char_dict[len(self.char_dict)] = chr(i)
+
+        self.char_token_dict = {}
+        for k, v in self.char_dict.items():
+            self.char_token_dict[v] = k
+
+    def tokenize(self, text):
+        text = normalize(text)
+        tokens = []
+        tokens.append(0) # append CLS token default as BOS
+
+        for char in text:
+            if 97 <= ord(char) <=122 or 44032 <= ord(char) <= 55203:
+                tokens.append(self.char_token_dict[char])
+            else:
+                tokens.append(4) #unknown token
+
+        tokens.append(1) # append SEP token default as EOS
+
+        if len(tokens) < self.max_len:
+            tokens +=  ([2] * (sel.max_len - len(tokens)))
+
+
 
