11e03ad2a97bf981d288b5bcbfe916f4f021f7fd
Ubuntu <ltech@rasalt-dev.p1nvgwc5saqe3ass43yrm55tjc.syx.internal.cloudapp.net>
2020-09-14 03:15:22 +0000

    modify kor_char_tokenizer decode func

--- nlu_flow/response_generation/chitchat_response_generation/torch_transformer_model/inferencer.py
+++ nlu_flow/response_generation/chitchat_response_generation/torch_transformer_model/inferencer.py
@@ -1,28 +1,50 @@
 from fastapi import FastAPI
 
+from embedding_transformer import EmbeddingTransformer
+
 from nlu_flow.preprocessor.text_preprocessor import normalize
+from nlu_flow.utils.kor_char_tokenizer import KorCharTokenizer
 
 import torch
 
 app = FastAPI()
 is_ready = False
 
-#load domain_classifer_model
+# load chitchat_response_model
 model = None
 
-#endpoints
+tokenizer = KorCharTokenizer()
+model = EmbeddingTransformer(
+    vocab_size=tokenizer.get_vocab_size(),
+    max_seq_len=tokenizer.get_seq_len(),
+    pad_token_id=tokenizer.get_pad_token_id(),
+)
+model.load_state_dict(torch.load('transformer_chitchat_response_model.modeldict',  map_location=lambda storage, loc: storage))
+
+if model is not None:
+    is_ready = True
+
+print ('model load success')
+
+# endpoints
 @app.get("/")
 async def health():
     if is_ready:
-        output = {'code': 200}
+        output = {"code": 200}
     else:
-        output = {'code': 500}
+        output = {"code": 500}
     return output
 
+
 @app.post("/chitchat_response_generator/generate")
-async def predict_domain(text: str):
-    name = model.predict([normalize(text)])[0]
-    confidence = model.predict_proba([normalize(text)])[0].max()
+async def generate_response(text: str):
+    max_len = model.max_seq_len
+    tokens = tokenizer.tokenize(text)
 
-    return {'name': name, 'confidence': confidence, 'Classifier': 'domain_classifier_model.svc'}
+    pred = model(torch.LongTensor(tokens).unsqueeze(0))
+    response = tokenizer.decode(pred)
 
+    return {
+        "response": response,
+        "Generator": "transformer_chitchat_response_generator.modeldict",
+    }
