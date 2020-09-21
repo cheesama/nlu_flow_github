fix wrong loss_fn formula

nlu_flow/retrieval/chitchat_response_retrieval/koelectra_fine_tuner.py
@@ -34,9 +34,9 @@ class KoelectraQAFineTuner(nn.Module):
         neg_pair = (label != label.transpose(1,0)).float()
 
         #ignore negative similarity of negative pair
-        neg_loss = (1 + (neg_pair * sim_value)).clamp(min=0.0).mean()
+        neg_loss = (neg_pair * sim_value).clamp(min=0.0).mean()
         pos_loss = (1 - (pos_pair * sim_value)).mean()
-        loss = 1 + neg_loss - pos_loss
+        loss = neg_loss + pos_loss
 
         return loss, pos_loss, neg_loss
 