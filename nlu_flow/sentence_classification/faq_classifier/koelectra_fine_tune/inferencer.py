d09f79dc98907fb405586d7107c97249f91e9c9c
cheesama <cheehoon12@hanmail.net>
2020-10-26 10:59:36 +0900

    add META_ENPOINT ENV in faq-classifier dockerfile

--- nlu_flow/sentence_classification/faq_classifier/koelectra_fine_tune/inferencer.py
+++ nlu_flow/sentence_classification/faq_classifier/koelectra_fine_tune/inferencer.py
@@ -25,15 +25,5 @@ async def health():
 
 @app.post("/predict")
 async def predict_faq(text: str, top_k=3):
-    #name = model.predict([normalize(text, with_space=True)])[0]
-    #confidence = model.predict_proba([normalize(text)])[0].max()
-    probs = model.predict_proba([text])
-    result = sorted( zip( model.classes_, probs[0] ), key=lambda x:x[1], reverse=True )[-top_k:]
-    dict_result = []
-    for each_result in result:
-        each_dict = {'faq_intent': each_result[0], 'confidence': each_result[1], 'classifier': 'faq_classifier_model.rf'}
-        each_dict.update(response_dict[each_result[0]])
-        dict_result.append(each_dict)
-        
-    return dict_result
+    return model.inference(text, top_k)
 
