b51e1222d33f7328db4e57b1ddde12769d7a4a7e
cheesama <cheehoon12@hanmail.net>
2020-11-04 12:56:46 +0900

    modify req of ludwig which exclude horovod setting

--- nlu_flow/sentence_classification/scenario_classifier/ludwig_fine_tune/train_val.py
+++ nlu_flow/sentence_classification/scenario_classifier/ludwig_fine_tune/train_val.py
@@ -26,6 +26,8 @@ for data in tqdm(synonym_data, desc=f"collecting synonym data for data augmentat
 
 utterances = []
 labels = []
+test_utterances = []
+test_labels = []
 
 ## scenario dataset preparation
 for scenario_table in scenario_table_list:
@@ -41,6 +43,9 @@ for scenario_table in scenario_table_list:
             if data['data_type'] == 'training':
                 utterances.append(data["utterance"]))
                 labels.append(data['intent_id']['Intent_ID'])
+            elif data['data_type'] == 'golden_set':
+                test_utterances.append(data["utterance"]))
+                test_labels.append(data['intent_id']['Intent_ID'])
 
                 #synonym augmentation
                 if len(data['entities']) == 1:
@@ -58,8 +63,18 @@ with open('scenario_dataset.tsv', 'w') as scenarioData:
         scenarioData.write(labels[i].strip().replace('\t', ' '))
         scenarioData.write('\n')
 
+with open('scenario_test_dataset.tsv', 'w') as scenarioData:
+    scenarioData.write('text\tclass\n')
+    
+    for i, utter in enumerate(test_utterances):
+        scenarioData.write(utter.strip().replace('\t',' '))
+        scenarioData.write('\t')
+        scenarioData.write(test_labels[i].strip().replace('\t', ' '))
+        scenarioData.write('\n')
+
 os.system('rm -rf results')
 os.system('ludwig experiment --dataset scenario_dataset.tsv --config_file config.yml')
+os.system('ludwig predict --dataset scenario_test_dataset.tsv --model_path results/experiment_run/model')
 
 #write result to file
 with open('results/experiment_run/test_statistics.json') as f:
