36148ddb730ffd42944d3a976460e3592e806bbc
cheesama <cheehoon12@hanmail.net>
2020-11-06 21:15:40 +0900

    add python 3.8 setup step in script 2

--- external/gpu-host-setting.sh
+++ external/gpu-host-setting.sh
@@ -1,6 +1,9 @@
 #install python 3.8 version
 sudo apt-get install build-essential checkinstall
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+cd /opt
+sudo wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz
+sudo tar xzf Python-3.8.5.tgz
 cd Python-3.8.5
 sudo ./configure --enable-optimizations
 sudo make altinstall
